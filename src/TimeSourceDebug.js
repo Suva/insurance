f711d65b360f964fa3675ba5bb8ffed44618fb24
Jaan Pullerits <clysuva@gmail.com>
2014-04-07 20:07:51 +0300

    Add keyboard event simulator.

--- src/TimeSourceDebug.js
+++ src/TimeSourceDebug.js
@@ -1,6 +1,18 @@
 define(function(){
     var startTime = 0;
+    var events = [];
 
+    $(window).on("keydown", function(ev){
+        console.log(ev.keyCode);
+        switch(ev.keyCode){
+            case 90: // Z key
+                events.push({instrument: 1, note: 'C-3'}); // Kick
+                break;
+            case 88: // X key
+                events.push({instrument: 1, note: 'D-3'}); // Snare
+                break;
+        }
+    });
 
     return {
         start: function(){
@@ -10,7 +22,12 @@ define(function(){
             return getCurrentTime() - startTime;
         },
         getEvents: function(){
-            return [];
+            if(events.length == 0){
+                return [];
+            }
+            var retEvents = _.clone(events);
+            events = [];
+            return retEvents;
         }
     };
 

e2957224ab17ebd521619c107566e123fe9a72b3
Jaan Pullerits <clysuva@gmail.com>
2014-04-12 15:37:49 +0300

    Fix issues with the camera being too far from screen plane so corners are visible.

--- src/scene/BridgeScene.js
+++ src/scene/BridgeScene.js
@@ -5,7 +5,7 @@ define(function(require){
 
     var screen = new THREE.Object3D();
     var layer1 = new THREE.Mesh(
-        new THREE.PlaneGeometry(13.5, 9),
+        new THREE.PlaneGeometry(15, 10),
         new THREE.MeshBasicMaterial({
             map: THREE.ImageUtils.loadTexture("images/cockpit_01.png"),
             transparent: true
@@ -13,31 +13,31 @@ define(function(require){
     );
 
     var layer2 = new THREE.Mesh(
-        new THREE.PlaneGeometry(13.5, 9),
+        new THREE.PlaneGeometry(15, 10),
         new THREE.MeshBasicMaterial({
             map: THREE.ImageUtils.loadTexture("images/cockpit_02.png"),
             transparent: true
         })
     );
-    layer2.position.z = 0.1;
+    layer2.position.z = 0.001;
 
     var layer3 = new THREE.Mesh(
-        new THREE.PlaneGeometry(13.5, 9),
+        new THREE.PlaneGeometry(15, 10),
         new THREE.MeshBasicMaterial({
             map: THREE.ImageUtils.loadTexture("images/cockpit_03.png"),
             transparent: true
         })
     );
-    layer3.position.z = 0.2;
+    layer3.position.z = 0.002;
 
     var layer4 = new THREE.Mesh(
-        new THREE.PlaneGeometry(13.5, 9),
+        new THREE.PlaneGeometry(15, 10),
         new THREE.MeshBasicMaterial({
             map: THREE.ImageUtils.loadTexture("images/cockpit_04.png"),
             transparent: true
         })
     );
-    layer4.position.z = 0.3;
+    layer4.position.z = 0.003;
 
     screen.add(layer1);
     screen.add(layer2);
@@ -61,10 +61,8 @@ define(function(require){
 
     scene.add(title);
 
-    camera.position.z = 5;
-    camera.lookAt(new THREE.Vector3());
-
-    var texPos = 0;
+    camera.position.z = 5.4;
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
 
     return {
         scene: scene,

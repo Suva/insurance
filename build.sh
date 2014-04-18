47faa753cdc8f6aec4d448cdd3d643e3f09abbcd
Jaan Pullerits <clysuva@gmail.com>
2014-04-18 18:59:26 +0200

    Add nfo file

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 rm -f cybercat-insurance.zip
 mkdir cybercat-insurance
 
-cp -r bower_components images lib models music src index.html cybercat-insurance
+cp -r bower_components images lib models music src index.html cybercat.nfo cybercat-insurance
 
-zip cybercat-insurance.zip -r cybercat-insurance
+zip cybercat-insurance.zip -9 -r cybercat-insurance
 
 rm -rf cybercat-insurance/
